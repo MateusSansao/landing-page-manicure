07f57c02283597620bb4b177cacc152eb8bd3e85
MateusSansão <mateuss12354678@gmail.com>
2025-03-12 22:02:26 -0300

    push

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ export function Footer() {
                 MARCELLA VIEIRA
             </h1>
             <p>
-                Marcella Vieira é especialista em unhas naturais e criadora do Método Marcella Vieira, uma metodologia exclusiva que ensina manicures a alcançarem um acabamento impecável, fidelizarem clientes e se destacarem no mercado da beleza. Com mais de 12 anos de experiência na área da beleza e 6 anos como Nail Designer, Marcella já dominou toda as técnicas disponíveis no mercado, mas escolheu se especializar em unhas naturais, trabalhando exclusivamente com elas há mais de 3 anos.<br/> 
+                Marcella Vieira é especialista em unhas naturais e criadora do Método Marcella Vieira, uma metodologia exclusiva que ensina manicures a alcançarem um acabamento impecável, fidelizarem clientes e se destacarem no mercado da beleza. Com mais de 12 anos de experiência na área da beleza e 6 anos como Nail Designer, Marcella já dominou todas as técnicas disponíveis no mercado, mas escolheu se especializar em unhas naturais, trabalhando exclusivamente com elas há mais de 3 anos.<br/> 
                 E também atua a mais de 3 anos na área do ensino, ajudando outras profissionais a crescerem e conquistarem o reconhecimento que merecem.<br/> 
                 Seu método já transformou a vida de mais de 800 profissionais pelo Brasil, ensinando técnicas avançadas, estratégias para valorização profissional e um passo a passo completo para quem quer evoluir na profissão e conquistar clientes fiéis... 
             </p>
